7cb42dceaf108240548dc830fe3a1c1a77ba44dd
vladfreishmidt <vladfreishmidt.webdevelopment@gmail.com>
2023-09-01 14:39:12 +0300

    Add WhenGrownUp func

---
+++ dog.go
@@ -0,0 +1,7 @@
+package dog
+
+import "strings"
+
+func WhenGrownUp(s string) string {
+	return "When the puppy grown up it says " + strings.ToUpper(s)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/vladfreishmidt/dog
+
+go 1.21.0
